24a2a8060776ffa697ff913bf2fa3938fad84fcc
Lwin Moe Naing <lwinmoenaing1331@gmail.com>
2021-05-26 15:33:33 +0630

    update code

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -5,7 +5,7 @@
    <!-- posts -->
    
    <div class="container">
-      <h1 class="mt-3">All Posts</h1>
+      <h1 class="mt-3">All Posts on Social App</h1>
       <div class="row">
       @foreach ($posts as $post )
           <div class="col-md-4 mt-3">
